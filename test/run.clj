9352629c984dc1f665bffed16617d5d7e6f850d1
Kresimir Sojat <Kresimir Sojat@localhost>
2009-09-25 21:08:31 +0200

    Added scripts for xecuting Truba unit tests.

---
+++ test/run.clj
@@ -0,0 +1,26 @@
+;; Copyright (c) Krešimir Šojat, 2009. All rights reserved. The use
+;; and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this
+;; distribution. By using this software in any fashion, you are
+;; agreeing to be bound by the terms of this license. You must not
+;; remove this notice, or any other, from this software.
+
+(require 'clojure.test)
+
+(def tests
+  [])
+
+(defn run-tests []
+  (let [found-ns (atom [])]
+    (doseq [t tests]
+      (try
+        (require :reload-all t)
+        (swap! found-ns conj t)
+
+        (catch Exception e
+          (println (.getMessage e)))))
+
+    (apply clojure.test/run-tests @found-ns)))
+
+(run-tests)
