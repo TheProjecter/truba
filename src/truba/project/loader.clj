964537533e0db15b6fd03fe3b7e268c2fd5e81c6
Kresimir Sojat <Kresimir Sojat@localhost>
2009-10-21 18:40:15 +0200

    Added first version of build file loader.

---
+++ src/truba/project/loader.clj
@@ -0,0 +1,44 @@
+;; Copyright (c) Krešimir Šojat, 2009. All rights reserved. The use
+;; and distribution terms for this software are covered by the Eclipse
+;; Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this
+;; distribution. By using this software in any fashion, you are
+;; agreeing to be bound by the terms of this license. You must not
+;; remove this notice, or any other, from this software.
+
+(ns
+  #^{:author "Krešimir Šojat"
+     :license {:name "Eclipse Public License 1.0"
+               :url  "http://opensource.org/licenses/eclipse-1.0.php"}}
+  truba.build.loader
+  (:import (java.io File)
+           (clojure.lang Compiler))
+  (:use [truba.build.collector :only [with-collector file-collector]]))
+
+(defn load-buildfile [file]
+  (let [b-name (gensym "trubafile_")
+        b-ns   (create-ns b-name)]
+    (binding [*ns* b-ns]
+      ; Add clojure.core
+      (refer-clojure)
+
+      ; Standard imports
+      (import '(java.io File))
+
+      ; Truba parts initialy available in trubafile.
+      (use
+        '(truba.build
+           [property :only [property properties]]
+           [task :only [task]]
+           [generator :only [generator]]))
+
+      ; Add basedir of trubafile to classpath.
+      (let [dir (.. (File. file) getParentFile toURI)]
+        (add-classpath dir))
+
+      (merge
+        (with-collector file-collector
+          (Compiler/loadFile file))
+        {:Finalizer
+          (fn []
+            (remove-ns b-name))}))))
